9f890aad7d402ed890f87267ea374d622c31572b
RomKnudsen <andreasrom@hotmail.com>
2023-12-07 04:46:08 -0800

    first commit

--- src/app/(site)/page.js
+++ src/app/(site)/page.js
@@ -1,14 +1,102 @@
-"use client";
-import Image from 'next/image'
-import Link from 'next/link'
-import {useLayoutEffect} from 'react';
-import {redirect} from 'next/navigation';
+// "use client";
+// import Image from 'next/image'
+// import Link from 'next/link'
+// import {useLayoutEffect} from 'react';
+// import {redirect} from 'next/navigation';
 
-export default function Home() {
-  useLayoutEffect(() => {
-      redirect('/login')
-  },[]);
-  return (<>
-    <h1>Home Page</h1>
-  </>)
+// export default function Home() {
+//   useLayoutEffect(() => {
+//       redirect('/login')
+//   },[]);
+//   return (<>
+//     <h1>Home Page</h1>
+//   </>)
+// }
+
+
+"use client";
+import React, { useState,useEffect } from 'react'
+import Image from 'next/image'
+import {MdAlternateEmail,MdKey} from 'react-icons/md'
+import Link from 'next/link'
+import axios from 'axios'
+import {useSelector,useDispatch} from 'react-redux';
+import {redirect} from 'next/navigation';
+import {showMessage,showError,clearMessage,clearError} from '@/utils/showAlert'
+
+const page = () => {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [loading,setLoading] = useState(false);
+    const {isAuth,user} = useSelector(store => store.user);
+    const dispatch = useDispatch();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true)
+        try {
+            const {data} = await axios.post('/api/v1/login',{email,password});
+            dispatch({type: 'loadUserSuc',payload: data});
+            await dispatch(showMessage(data.message));
+            await dispatch(clearMessage());
+            console.log(data.message);
+        } catch (error) {
+            await dispatch(showError(error.response.data.message));
+            await dispatch(clearError());
+            console.log(error.response.data.message)
+        }
+        setLoading(false);
+
+    }
+
+    useEffect(() => {
+        if(isAuth === true && user){
+            if(user?.isSubscriber){
+                redirect('/dashboard');
+            }else{
+                redirect('/subscribe');
+            }
+        }
+    },[isAuth,user])
+
+
+  return (
+    <section className='login-section h-screen'>
+        <div className='container m-auto h-full flex justify-center items-center'>
+        <div className='form-outer-box max-w-[40rem] w-[40rem] shadow-xl rounded-md p-2 py-4 border border-gray-100'>
+                <div className='py-4 flex justify-center items-center'>
+                    <Image src="/images/logo.svg" width={200} height={300} className='w-28'/>
+                </div>
+                <form className='p-3 px-6' onSubmit={handleSubmit}>
+                    <div className='input-group flex flex-col gap-1 mb-6'>
+                        <label for="email" className='text-black text-lg'>Email</label>
+                        <div className='flex items-center relative  py-2 px-1 border-gray-400  border-2 hover:border-indigo-500 rounded-md'>
+                            <MdAlternateEmail size={20} className='text-gray-400'/>
+                            <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} className='w-[95%] outline-none ml-1' placeholder='Enter your email' id='email' name='email' required/>
+                        </div>
+                    </div>
+
+                    <div className='input-group flex flex-col gap-1 mb-6'>
+                        <label for="password" className='text-black text-lg'>Password</label>
+                        <div className='flex items-center relative py-2 px-1 border-gray-400  border-2 hover:border-indigo-500 rounded-md'>
+                            <MdKey size={20} className='text-gray-400'/>
+                            <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} className='w-[95%] outline-none ml-1' placeholder='Enter your password' id='password' name='password' required/>
+                        </div>   
+                    </div>
+
+                    <div className='input-group flex items-center justify-between mb-6'>
+                        <p className='text-lg text-black'>If you {`don't`} have account <Link className='text-indigo-500 hover:underline' href={'/register'}>Sign Up</Link></p>  
+                        <p className='text-lg text-black'><Link className='text-indigo-500 hover:underline' href={'/forgot-password'}>Forgot Password</Link></p>  
+                    </div>
+
+                    <div className='flex justify-center items-center'>
+                        <button type='submit' className='py-2 px-4 rounded-md bg-indigo-500 text-white text-lg hover:bg-indigo-700 transition-all'>{!loading ? 'Login' : 'Loading...'}</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+  )
 }
+
+export default page
